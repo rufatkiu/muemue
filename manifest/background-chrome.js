c55f4100334e6341871b01939a8b3705508a2617
永远的极左派 <rufatkiu@mail.ru>
2023-06-20 12:33:08 +0800

    Update background-chrome.js

--- manifest/background-chrome.js
+++ manifest/background-chrome.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-chrome.runtime.setUninstallURL('https://muetab.com/uninstall');
+chrome.runtime.setUninstallURL('https://star.metastem.su/uninstall');
 
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {

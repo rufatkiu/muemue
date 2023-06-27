2942f391a2f2a535a8f5713287321c5fd2fcc4c7
永远的极左派 <rufatkiu@mail.ru>
2023-06-27 12:40:42 +0800

    Update constants.js

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -2,17 +2,17 @@
 export const API_URL = 'https://api.metastem.su';
 export const PROXY_URL = 'https://proxy.metastem.su';
 export const MARKETPLACE_URL = 'https://marketplace.metastem.su';
-export const SPONSORS_URL = 'https://sponsors.muetab.com';
+export const SPONSORS_URL = 'https://sponsors.metastem.su';
 export const GITHUB_URL = 'https://api.github.com';
 export const DDG_IMAGE_PROXY = 'https://external-content.duckduckgo.com/iu/?u=';
 export const MAPBOX_URL = 'https://api.mapbox.com';
 export const OPENSTREETMAP_URL = 'https://www.openstreetmap.org';
 
 // Mue URLs
-export const WEBSITE_URL = 'https://muetab.com';
-export const PRIVACY_URL = 'https://muetab.com/privacy';
-export const BLOG_POST = 'https://blog.muetab.com/posts/version-6-0';
-export const TRANSLATIONS_URL = 'https://docs.muetab.com/translations/';
+export const WEBSITE_URL = 'https://metastem.su';
+export const PRIVACY_URL = 'https://metastem.su/privacy';
+export const BLOG_POST = 'https://blog.metastem.su/';
+export const TRANSLATIONS_URL = 'https://docs.metastem.su/translations/';
 
 // Mue Info
 export const ORG_NAME = 'Metastem';
@@ -23,10 +23,10 @@ export const DISCORD_SERVER = '';
 export const COPYRIGHT_NAME = 'Metastem Commune';
 export const COPYRIGHT_YEAR = '2019';
 export const COPYRIGHT_LICENSE = 'BSD-3-Clause License';
-export const SPONSORS_USERNAME = 'Metastem Commune';
+export const SPONSORS_USERNAME = 'Metastem';
 export const LIBERAPAY_USERNAME = 'Metastem';
-export const KOFI_USERNAME = 'Metastem Commune';
-export const PATREON_USERNAME = 'Metastem Commune';
+export const KOFI_USERNAME = '';
+export const PATREON_USERNAME = '';
 
 export const OFFLINE_IMAGES = 20;
 
